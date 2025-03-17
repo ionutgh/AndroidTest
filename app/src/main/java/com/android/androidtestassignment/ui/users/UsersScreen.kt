71705383631e6119cc8ad3cf14345f434c3676ee
Ionut Ghitescu <>
2025-03-17 12:04:13 +0200

    Created basic android project Added dependencies for retrofit, hilt and coil Created a basic users screen

---
+++ app/src/main/java/com/android/androidtestassignment/ui/users/UsersScreen.kt
@@ -0,0 +1,75 @@
+package com.android.androidtestassignment.ui.users
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.androidtestassignment.R
+import com.android.androidtestassignment.ui.theme.AndroidTestAssignmentTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UsersScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.title_users))
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = null
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.padding(innerPadding).fillMaxSize()
+        ) {
+            Text(
+                text = "Hello there",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UsersPreview() {
+    AndroidTestAssignmentTheme {
+        UsersScreen()
+    }
+}
